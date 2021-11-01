3ffc00832ac887aeef2167a7714ac0b68ee53249
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-11-01 13:03:53 +0100

    feat: Create Player & Song  components basic markup, add font awesome into project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react'
+// Adding Component to App
+import Song from './components/Song'
+import Player from './components/Player'
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song/>
+      <Player/>
     </div>
   );
 }

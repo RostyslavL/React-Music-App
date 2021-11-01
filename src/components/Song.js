9d95964ff08b886d8eda7e1394ad075a3bc10068
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-11-01 21:11:15 +0100

    fix:Bug fixed

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Song = ({currentSong}) => {
     return (
         <div className="song-container">
-            <img src={currentSong.cover}></img>
+            <img alt={currentSong.name} src={currentSong.cover}></img>
             <h1>{currentSong.artist}</h1>
             <h2>{currentSong.name}</h2>
             <h3>{currentSong.style}</h3>

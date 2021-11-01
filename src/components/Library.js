15cb1bcee29df63eb411250c198f0e840864c69e
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-11-01 22:14:56 +0100

    feat:Add icon with styles

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import LibrarySong from './LibrarySong'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompactDisc} from '@fortawesome/free-solid-svg-icons'
 
 const Library = ({songs}) => {
     return (
-        <div className="library">
-            <h2>Library</h2>
+        <div className="library">            
+            <h2>
+                <FontAwesomeIcon size="3x" icon={faCompactDisc}/>&nbsp;
+                Library
+            </h2>
             <div className="library-songs">
                 {songs.map((song) => (
                     <LibrarySong song={song}/>
